398fd2dfeede6957c45031f95d20d78a6e0e9d60
Jinghui Hu <hujinghui@buaa.edu.cn>
2017-05-31 14:06:43 +0800

    formatting codes

--- lubksbBO.c
+++ lubksbBO.c
@@ -7,23 +7,23 @@
 
 void lubksbBO( double ** AA, int nIn, int * indx, double bb[])
 {
-  int ii = -1;
+    int ii = -1;
 
-  for( int i = 0; i < nIn; i++ ) {
-    int ip = indx[i];
-    double sum = bb[ip];
-    bb[ip] = bb[i];
-    if( ii != -1 ) {
-      for( int j = ii; j <= i-1; j++ ) sum -= AA[i][j]*bb[j];
+    for( int i = 0; i < nIn; i++ ) {
+        int ip = indx[i];
+        double sum = bb[ip];
+        bb[ip] = bb[i];
+        if( ii != -1 ) {
+            for( int j = ii; j <= i-1; j++ ) sum -= AA[i][j]*bb[j];
+        }
+        else if( sum ) {
+            ii = i;
+        }
+        bb[i] = sum;
     }
-    else if( sum ) {
-      ii = i;
+    for( int i = nIn-1; i >= 0; i-- ) {
+        double sum = bb[i];
+        for( int j = i+1; j < nIn; j++ ) sum -= AA[i][j]*bb[j];
+        bb[i] = sum/AA[i][i];
     }
-    bb[i] = sum;
-  }
-  for( int i = nIn-1; i >= 0; i-- ) {
-    double sum = bb[i];
-    for( int j = i+1; j < nIn; j++ ) sum -= AA[i][j]*bb[j];
-    bb[i] = sum/AA[i][i];
-  }
 }
--- ludcmpBO.c
+++ ludcmpBO.c
@@ -11,57 +11,57 @@
 
 void ludcmpBO( double** AA, int nIn, int* indx, double* dd)
 {
-  double dum, sum, temp;
-  int imax = 0;
+    double dum, sum, temp;
+    int imax = 0;
 
-  double* vv = (double *) malloc( (size_t) (nIn * sizeof(double)) );
-  if (!vv) fprintf(stdout,"Allocation failure in ludcmpBO\n");
+    double* vv = (double *) malloc( (size_t) (nIn * sizeof(double)) );
+    if (!vv) fprintf(stdout,"Allocation failure in ludcmpBO\n");
 
-  *dd = 1.0;
-  for( int i = 0; i < nIn; i++ ) {
-    double big = 0.0;
+    *dd = 1.0;
+    for( int i = 0; i < nIn; i++ ) {
+        double big = 0.0;
+        for( int j = 0; j < nIn; j++ ) {
+            if ((temp = fabs(AA[i][j])) > big) big = temp;
+        }
+        if (big == 0.0) fprintf(stdout,"Singular matrix in routine ludcmp!\n");
+        vv[i] = 1.0 / big;
+    }
+
     for( int j = 0; j < nIn; j++ ) {
-      if ((temp = fabs(AA[i][j])) > big) big = temp;
+        for( int i = 0; i < j; i++ ) {
+            sum = AA[i][j];
+            for( int k = 0; k < i; k++) sum -= AA[i][k]*AA[k][j];
+            AA[i][j] = sum;
+        }
+        double big = 0.0;
+        for( int i = j; i < nIn; i++ ) {
+            sum = AA[i][j];
+            for( int k = 0; k < j; k++ ) {
+                sum -= AA[i][k]*AA[k][j];
+            }
+            AA[i][j] = sum;
+            if( (dum = vv[i]*fabs(sum)) >= big ) {
+                big = dum;
+                imax = i;
+            }
+        }
+        if( j != imax ) {
+            for( int k = 0; k < nIn; k++ ) {
+                dum = AA[imax][k];
+                AA[imax][k] = AA[j][k];
+                AA[j][k] = dum;
+            }
+            *dd = -(*dd);
+            vv[imax] = vv[j];
+        }
+        indx[j] = imax;
+        if( AA[j][j] == 0.0 ) AA[j][j] = TINY;
+        if( j != nIn ) {
+            dum = 1.0 / AA[j][j];
+            for( int i = j+1; i < nIn; i++ ) AA[i][j] *= dum;
+        }
     }
-    if (big == 0.0) fprintf(stdout,"Singular matrix in routine ludcmp!\n");
-    vv[i] = 1.0 / big;
-  }
 
-  for( int j = 0; j < nIn; j++ ) {
-    for( int i = 0; i < j; i++ ) {
-      sum = AA[i][j];
-      for( int k = 0; k < i; k++) sum -= AA[i][k]*AA[k][j];
-      AA[i][j] = sum;
-    }
-    double big = 0.0;
-    for( int i = j; i < nIn; i++ ) {
-      sum = AA[i][j];
-      for( int k = 0; k < j; k++ ) {
-        sum -= AA[i][k]*AA[k][j];
-      }
-      AA[i][j] = sum;
-      if( (dum = vv[i]*fabs(sum)) >= big ) {
-        big = dum;
-        imax = i;
-      }
-    }
-    if( j != imax ) {
-      for( int k = 0; k < nIn; k++ ) {
-        dum = AA[imax][k];
-        AA[imax][k] = AA[j][k];
-        AA[j][k] = dum;
-      }
-      *dd = -(*dd);
-      vv[imax] = vv[j];
-    }
-    indx[j] = imax;
-    if( AA[j][j] == 0.0 ) AA[j][j] = TINY;
-    if( j != nIn ) {
-      dum = 1.0 / AA[j][j];
-      for( int i = j+1; i < nIn; i++ ) AA[i][j] *= dum;
-    }
-  }
-
-  free( (FREE_ARG) (vv) );
+    free( (FREE_ARG) (vv) );
 
 }
